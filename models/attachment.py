8b656aeb78b2e7ed3cd598a3ce7989cae6d12c7d
sara-khaled90 <56353610+sara-khaled90@users.noreply.github.com>
2026-06-25 20:02:56 +0300

    Update attachment.py

--- models/attachment.py
+++ models/attachment.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 # Part of Softhealer Technologies.
 
-from odoo import models,fields,api,_
+from odoo import models, fields, api, _
 import random
 
 class Attachment(models.Model):
-    _inherit='ir.attachment'
+    _inherit = 'ir.attachment'
     
-    directory_id = fields.Many2one('document.directory',string='Directory')
-    document_tags = fields.Many2many('document.tags',string='Document Tags')
+    directory_id = fields.Many2one('document.directory', string='Directory')
+    document_tags = fields.Many2many('document.tags', string='Document Tags')
     color = fields.Integer(string='Color Index')
     
+    # الحقل الجديد لاختيار أي عدد من المستخدمين لمشاركة المستند معهم
+    shared_user_ids = fields.Many2many(
+        'res.users', 
+        'sh_document_res_users_rel', # اسم جدول الربط في قاعدة البيانات
+        'attachment_id', 
+        'user_id', 
+        string='Shared With'
+    )
     
     @api.model
     def create(self, values):
